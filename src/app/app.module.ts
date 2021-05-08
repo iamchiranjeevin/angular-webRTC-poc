af8d93bba2490ac1d260e334e216d427d21907f2
ChiranjeeviN <nchiranjeevi22@gmail.com>
2021-05-08 16:47:35 +0530

    initial commit - enabled access to local video stream

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatGridListModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,47 @@
+import { AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+const mediaConstraints = {
+  audio: true,
+  video: { width: 720, height: 540 }
+}
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit, AfterViewInit {
+
+  private localStream: MediaStream;
+  @ViewChild('local_video') localVideo: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.requestMediaDevices();
+  }
+
+  private async requestMediaDevices(): Promise<void> {
+    this.localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+    this.pauseLocalVideo();
+  }
+
+  pauseLocalVideo() {
+    this.localStream.getTracks().forEach(track => {
+      track.enabled = false;
+    });
+    this.localVideo.nativeElement.srcObject = undefined;
+  }
+
+  startLocalVideo() {
+    this.localStream.getTracks().forEach(track => {
+      track.enabled = true;
+    });
+    this.localVideo.nativeElement.srcObject = this.localStream;;
+  }
+
+}
