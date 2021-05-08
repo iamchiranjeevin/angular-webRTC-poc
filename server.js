692c4e888da92a5276e24d0b1492e2fe9223472b
ChiranjeeviN <nchiranjeevi22@gmail.com>
2021-05-08 23:52:13 +0530

    changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-web-rtc-poc'));
+app.use(express.static(__dirname + '/dist/angular-webRTC-poc'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/angular-web-rtc-poc/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-webRTC-poc/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
